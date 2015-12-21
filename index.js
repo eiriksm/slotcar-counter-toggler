ca5b37777a0306d2abf4a9519e2c714cbce3bdf1
Eirik S. Morland <eirik@morland.no>
2015-12-21 19:07:58 +0100

    Move to serialport communication instead. Add speed and name functionality.

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 'use strict';
-var five = require('johnny-five');
-var board = new five.Board({
-  repl: false
+var path = require('path');
+var serialport = require('serialport');
+var SerialPort = serialport.SerialPort;
+var sp = new SerialPort('/dev/tty.usbmodem1411', {
+  baudrate: 9600,
+  parser: serialport.parsers.readline("\n")
 });
 var app = require('express')();
 var http = require('http').Server(app);
@@ -14,35 +17,50 @@ function logger() {
 }
 
 app.get('/', function(req, res){
-  res.sendfile('index.html');
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-function init(relay) {
+var name = 'player 1';
+
+function init() {
   io.on('connection', function(socket) {
+    socket.emit('name', name);
+    socket.currentScore = 0;
     logger('Client connected');
     socket.on('disconnect', function(){
       logger('Client disconnected');
     });
     socket.on('start', function(msg){
       logger('Start received');
+      io.emit('countdown');
       setTimeout(function() {
-        relay.toggle();
-        socket.emit('started');
+        logger('Toggle power');
+        socket.startTime = Date.now()
+        sp.write(new Buffer([0x01]));
+        io.emit('started');
       }, 3000);
     });
     socket.on('stop', function() {
-      relay.toggle();
-      socket.emit('end');
+      var score = Math.floor((Date.now() - socket.startTime) * Math.PI);
+      logger('Stop received. Score:', score);
+      io.emit('end', score);
+      socket.startTime = null;
+      sp.write(new Buffer([0x01]));
     });
+    socket.on('name', function(d) {
+      logger('New name', d);
+      name = d;
+    })
   });
 
   http.listen(3000, function(){
     logger('listening on *:3000');
   });
+  sp.on('open', function () {
+    logger('Serialport opened');
+    sp.on('data', function(d) {
+      io.emit('speed', d.toString())
+    });
+  });
 }
-
-board.on('ready', function() {
-  var relay = new five.Relay(10);
-  logger('Board ready');
-  init(relay);
-});
+init();
